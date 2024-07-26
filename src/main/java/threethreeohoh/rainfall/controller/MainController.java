52a1d23b8894050dfbd32d1c04a33e4dfef4d0ac
sunny.park <seolseee@gmail.com>
2024-07-26 18:46:14 +0900

    fix : Controller에 @CrossOrigin 어노테이션 추가

--- src/main/java/threethreeohoh/rainfall/controller/MainController.java
+++ src/main/java/threethreeohoh/rainfall/controller/MainController.java
@@ -3,6 +3,7 @@ package threethreeohoh.rainfall.controller;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.core.GrantedAuthority;
 import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -11,6 +12,7 @@ import java.util.Iterator;
 
 
 @RestController
+@CrossOrigin
 public class MainController {
 
     @GetMapping("/")
